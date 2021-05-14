fca5868c7c4eb315d800b4409c518f8dac4e34dd
magnus0506 <47506157+magnus0506@users.noreply.github.com>
2021-05-14 14:16:49 +0200

    navbar

---
+++ src/main/java/com/kea/culturecall/CulturecallApplication.java
@@ -0,0 +1,13 @@
+package com.kea.culturecall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulturecallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CulturecallApplication.class, args);
+    }
+
+}
