aa002a4969546e5bb36627c9b23bdbf67a17b628
jean591g <jean591g@stud.kea.dk>
2021-05-28 14:39:28 +0200

    Update Security test 01 - 28.05.2021 kl.14.39

--- src/main/java/com/kea/culturecall_frontend/config/SecurityConfig.java
+++ src/main/java/com/kea/culturecall_frontend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.kea.culturecall_frontend.config;
 
-import com.*;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.catalina.User;
